66fa9464fe4c7ca8dcd0070556a9f2e456536e24
Mohit Virli <virlimohit@gmail.com>
2025-06-09 21:27:37 +0530

    Added github backlink

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { motion } from "framer-motion";
+import Image from "next/image";
 import { useEffect } from "react";
+import CursorFollower from "./components/CursorFollower";
+import Magnet from "./components/Magnet";
 import { Progress } from "./components/Progress";
 import { useTimeStore } from "./stores/timeStore";
 import { Time } from "./Time";
-import CursorFollower from "./components/CursorFollower";
 import { formatTitle } from "./utils/title";
 
 export default function Home() {
@@ -24,6 +27,13 @@ export default function Home() {
       <CursorFollower/>
       <Time />
       <Progress />
+      <a href="https://github.com/mohitvirli/focus-clock" target="_blank" className="absolute top-4 right-4">
+        <Magnet magnetStrength={10}>
+          <motion.div whileHover={{ scale: 1.5 }} whileTap={{ scale: 0.9 }}>
+            <Image src="./github.svg" width="24" height="24" alt="github"/>
+          </motion.div>
+        </Magnet>
+      </a>
     </div>
   );
 }
